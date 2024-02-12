35fcb2896e73a845827b2719519b51bc4ff5b8c8
Samuel Mulford <samuelmulford@icloud.com>
2024-02-12 14:35:58 -0600

    3d box

--- Demo_Geometry_Shaders/README.md
+++ Demo_Geometry_Shaders/README.md
@@ -1,1 +1,8 @@
-# Geometry and Shaders
+# Geometry and shaders
+
+1. Navigate to Demo_Geometry_Shaders
+2. ```npm install```
+3. ```npm run start```
+4. Navigate to [http://localhost:3000](http://localhost:3000)
+
+**This demonstration code adapted from Invent Box on Youtube**
---
+++ Demo_Geometry_Shaders/public/main.js
@@ -0,0 +1,163 @@
+// Adapted from Invent Box on Youtube
+
+const canvas = document.querySelector('canvas');
+const gl = canvas.getContext('webgl');
+
+if (!gl) {
+    throw new Error('WebGL not supported');
+}
+
+// WebGL primarily works with triangles
+// 6 faces * 2 triangles / face = 12 triangles
+// 12 triangles * 3 vertices / triangle = 36 vertices
+// Only 8 unique vertices
+const vertexData = [
+
+    // Front
+    0.5, 0.5, 0.5,
+    0.5, -.5, 0.5,
+    -.5, 0.5, 0.5,
+    -.5, 0.5, 0.5,
+    0.5, -.5, 0.5,
+    -.5, -.5, 0.5,
+
+    // Left
+    -.5, 0.5, 0.5,
+    -.5, -.5, 0.5,
+    -.5, 0.5, -.5,
+    -.5, 0.5, -.5,
+    -.5, -.5, 0.5,
+    -.5, -.5, -.5,
+
+    // Back
+    -.5, 0.5, -.5,
+    -.5, -.5, -.5,
+    0.5, 0.5, -.5,
+    0.5, 0.5, -.5,
+    -.5, -.5, -.5,
+    0.5, -.5, -.5,
+
+    // Right
+    0.5, 0.5, -.5,
+    0.5, -.5, -.5,
+    0.5, 0.5, 0.5,
+    0.5, 0.5, 0.5,
+    0.5, -.5, 0.5,
+    0.5, -.5, -.5,
+
+    // Top
+    0.5, 0.5, 0.5,
+    0.5, 0.5, -.5,
+    -.5, 0.5, 0.5,
+    -.5, 0.5, 0.5,
+    0.5, 0.5, -.5,
+    -.5, 0.5, -.5,
+
+    // Bottom
+    0.5, -.5, 0.5,
+    0.5, -.5, -.5,
+    -.5, -.5, 0.5,
+    -.5, -.5, 0.5,
+    0.5, -.5, -.5,
+    -.5, -.5, -.5,
+];
+
+function randomColor() {
+    return [Math.random(), Math.random(), Math.random()];
+}
+
+let colorData = [];
+for (let face = 0; face < 6; face++) {
+    let faceColor = randomColor();
+    for (let vertex = 0; vertex < 6; vertex++) {
+        colorData.push(...faceColor);
+    }
+}
+
+// Buffers can store any type of WebGL data needed for the GPU to render a shape
+// Position buffer stores the vertex positions
+// gl.ARRAY_BUFFER stores vertex attributes
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+const colorBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
+
+// Process each vertex position and color
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertexShader, `
+precision mediump float;
+
+attribute vec3 position;
+attribute vec3 color;
+varying vec3 vColor;
+
+uniform mat4 matrix;
+
+void main() {
+    vColor = color;
+    gl_Position = matrix * vec4(position, 1);
+}
+`);
+gl.compileShader(vertexShader);
+
+// Fragment shader determines the color of each pixel
+// by interpolating the colors of the vertices
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragmentShader, `
+precision mediump float;
+
+varying vec3 vColor;
+
+void main() {
+    gl_FragColor = vec4(vColor, 1);
+}
+`);
+gl.compileShader(fragmentShader);
+console.log(gl.getShaderInfoLog(fragmentShader));
+
+const program = gl.createProgram();
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+
+gl.linkProgram(program);
+
+const positionLocation = gl.getAttribLocation(program, `position`);
+gl.enableVertexAttribArray(positionLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+const colorLocation = gl.getAttribLocation(program, `color`);
+gl.enableVertexAttribArray(colorLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
+
+gl.useProgram(program);
+gl.enable(gl.DEPTH_TEST);
+
+const uniformLocations = {
+    matrix: gl.getUniformLocation(program, `matrix`),
+};
+
+const matrix = mat4.create();
+
+// If you want to play with translations
+//mat4.translate(matrix, matrix, [.2, .5, 0]);
+
+mat4.scale(matrix, matrix, [0.5, 0.5, 0.5]);
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    mat4.rotateZ(matrix, matrix, Math.PI/2 / 200);
+    mat4.rotateX(matrix, matrix, Math.PI/2 / 200);
+    mat4.rotateY(matrix, matrix, Math.PI/2 / 200);
+
+    gl.uniformMatrix4fv(uniformLocations.matrix, false, matrix);
+    gl.drawArrays(gl.TRIANGLES, 0, vertexData.length / 3);
+}
+
+animate();
+
